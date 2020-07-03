1af02f7195a94477b23043638f57691a14ab4979
spin6lock <spin6lock@users.noreply.github.com>
2020-07-03 18:07:03 +0800

    add ctrl channel to close goroutine

--- main.go
+++ main.go
@@ -47,23 +47,61 @@ func handleRequest(conn net.Conn) {
         os.Exit(1)
     }
 	scon, _ := scp.Client(raw, nil)
+    defer scon.Close()
     plaintext_buf := make([]byte, 1024)
     scp_buf := make([]byte, 1024)
+    ch := make(chan string)
+    readCh := make(chan []byte)
+    writeCh := make(chan []byte)
+    go func() {
+        for {
+            size, err := conn.Read(plaintext_buf)
+            if err != nil {
+                log.Printf("conn read error:%s\n", err.Error())
+                break
+            }
+            data := plaintext_buf[:size]
+            log.Println("Read new data from connection", data)
+            readCh <- data
+        }
+        ch <- ""
+    }()
+    go func() {
+        for {
+            size, err := scon.Read(scp_buf)
+            if err != nil {
+                log.Printf("scon read error:%s\n", err.Error())
+                break
+            }
+            data := scp_buf[:size]
+            log.Println("Read data from scp:", data)
+            writeCh <- data
+        }
+        ch <- ""
+    }()
+    go readPipe(readCh, ch, scon)
+    go writePipe(writeCh, ch, conn)
+    <-ch
+}
+
+func readPipe(readCh chan []byte, ctrlCh chan string, scon *scp.Conn) {
 	for {
-		size, err := conn.Read(plaintext_buf)
-		if err != nil {
-			return
-		}
-		data := plaintext_buf[:size]
-		log.Println("Read new data from connection", data)
-        scon.Write(data)
-        size, err = scon.Read(scp_buf)
-		if err != nil {
-            log.Println("scon read error:%s", err.Error())
-			return
-		}
-		data = scp_buf[:size]
-        log.Println("Read data from scp:", data)
-        conn.Write(data)
-	}
+        select {
+        case data := <-readCh:
+            scon.Write(data)
+        case <-ctrlCh:
+            log.Println("readPipe got close")
+        }
+    }
+}
+
+func writePipe(writeCh chan []byte, ctrlCh chan string, conn net.Conn) {
+    for {
+        select {
+        case data := <-writeCh:
+            conn.Write(data)
+        case <-ctrlCh:
+            log.Println("writePipe got close")
+        }
+    }
 }
